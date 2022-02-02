2fe87bbb2ef95bd30d6a5a4c16e68b98122981f2
Joseph Nahm <josephnahm1@gmail.com>
2022-02-01 18:33:00 -0800

    ETL process completed

--- database.sql
+++ database.sql
@@ -9,34 +9,38 @@ DROP TABLE IF EXISTS features;
 
 
 CREATE TABLE products (
-  id SERIAL NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL,
-  slogan TEXT NOT NULL,
-  description TEXT NOT NULL,
-  category TEXT NOT NULL,
-  default_price INTEGER NOT NULL
+  id TEXT PRIMARY KEY,
+  name TEXT,
+  slogan TEXT,
+  description TEXT,
+  category TEXT,
+  default_price TEXT
 );
 
 CREATE TABLE features (
-  id SERIAL NOT NULL PRIMARY KEY,
-  product_id SERIAL REFERENCES products(id),
-  feature TEXT NOT NULL,
+  id TEXT PRIMARY KEY,
+  product_id TEXT REFERENCES products(id),
+  feature TEXT,
   "value" TEXT
 );
 
 CREATE TABLE styles (
-  id SERIAL NOT NULL PRIMARY KEY,
-  product_id SERIAL REFERENCES products (id),
-  "name" TEXT NOT NULL,
-  sale_price INTEGER,
-  original_price INTEGER,
-  default_style BOOLEAN NOT NULL
+  id TEXT PRIMARY KEY,
+  product_id TEXT REFERENCES products (id),
+  "name" TEXT,
+  sale_price TEXT,
+  original_price TEXT,
+  default_style TEXT
 );
 
 CREATE TABLE skus (
-  id SERIAL NOT NULL PRIMARY KEY,
-  style_id SERIAL REFERENCES styles (id),
+  id TEXT PRIMARY KEY,
+  style_id TEXT REFERENCES styles (id),
   size TEXT,
-  quantity INTEGER
+  quantity TEXT
 );
 
+\copy products FROM 'csv/product.csv' WITH (format csv, header);
+\copy features FROM 'csv/features.csv' WITH (format csv, header);
+\copy styles FROM 'csv/styles.csv' WITH (format csv, header);
+\copy skus FROM 'csv/skus.csv' WITH (format csv, header);
--- server/ETL.js
+++ server/ETL.js
@@ -1,36 +1,38 @@
-const fs = require("fs");
-const { Pool } = require("pg");
-const fastcsv = require("fast-csv");
+//Too slow
 
-var stream = fs.createReadStream("csv/example.csv");
-var csvData = [];
-var csvStream = fastcsv
-  .parse()
-  .on("data", (data) => csvData.push(data))
-  .on("end", () => {
-    csvData.shift();
+// const fs = require("fs");
+// const { Pool } = require("pg");
+// const fastcsv = require("fast-csv");
 
-    const pool = new Pool({
-      user: "josephnahm",
-      password: null,
-      host: "localhost",
-      port: 5432,
-      database: "overview",
-    });
+// var stream = fs.createReadStream("csv/product.csv");
+// var csvData = [];
+// var csvStream = fastcsv
+//   .parse()
+//   .on("data", (data) => csvData.push(data))
+//   .on("end", () => {
+//     csvData.shift();
 
-    const query =
-      "INSERT INTO features (product_id, feature, value) VALUES ($1, $2, $3)";
+//     const pool = new Pool({
+//       user: "josephnahm",
+//       password: null,
+//       host: "localhost",
+//       port: 5432,
+//       database: "overview",
+//     });
 
-    (async () => {
-      const client = await pool.connect();
-      try {
-        csvData.forEach(async (row) => {
-          const fields = [parseInt(row[1]), ...row.slice(2)];
-          const res = await client.query(query, fields);
-        });
-      } finally {
-        client.release();
-      }
-    })().catch((err) => console.log(err.stack));
-  });
-stream.pipe(csvStream);
+//     const query =
+//       "INSERT INTO products (name, slogan, description, category, default_price) VALUES ($1, $2, $3, $4, $5)";
+
+//     (async () => {
+//       const client = await pool.connect();
+//       try {
+//         csvData.forEach(async (row) => {
+//           const fields = [...row.slice(1, -1), parseInt(row[row.length - 1])];
+//           const res = await client.query(query, fields);
+//         });
+//       } finally {
+//         client.release();
+//       }
+//     })().catch((err) => console.log(err.stack));
+//   });
+// stream.pipe(csvStream);
