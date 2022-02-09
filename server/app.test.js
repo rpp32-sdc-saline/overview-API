3fe8c135046f997dcec8a17c30080f593f45bd0e
Joseph Nahm <josephnahm1@gmail.com>
2022-02-08 18:12:00 -0800

    testing implemented with 83% coverage

--- server/app.test.js
+++ server/app.test.js
@@ -1,22 +1,58 @@
 const request = require("supertest");
-const app = require("./app");
+const makeApp = require("./app");
+
+const getProducts = jest.fn();
+const getProduct = jest.fn();
+const getStyle = jest.fn();
+
+const app = makeApp({
+  getProducts,
+  getProduct,
+  getStyle,
+});
+
+beforeEach(() => {
+  getProducts.mockReset();
+});
 
 describe("GET /products", () => {
-  describe("given a username and password", () => {
-    test("No parameter passed in should return 5 json objects starting from page 1", async () => {
+  describe("request containing no parameters", () => {
+    test("No parameters should call getProducts with default values of page = 1 and count = 5", async () => {
       const response = await request(app).get("/products");
-      expect(response).toBe();
+      expect(getProducts.mock.calls.length).toBe(1);
+      expect(getProducts.mock.calls[0][0]).toBe(1);
+      expect(getProducts.mock.calls[0][1]).toBe(5);
     });
   });
 
-  // describe("Count parameter of 7 returns 7 json objects in response", () => {
-  //   test("No parameter passed in should return 5 json objects starting from page 1", async () => {
-  //     const response = await request(app).post("/products").send({
-  //       username: "username",
-  //       password: "password",
-  //     });
-  //     expect(response.statusCode).toBe(200);
-  //   });
-  // });
-  describe("Page parameter of 2 with count 7 returns 7 json objects in response starting from id of 14", () => {});
+  describe("request containing parameters", () => {
+    test("count parameter of 7 and page parameter of 7 should call getProducts with corresponding page and count arguments", async () => {
+      const response = await request(app)
+        .get("/products")
+        .query({ page: 7, count: 7 });
+      expect(getProducts.mock.calls.length).toBe(1);
+      expect(getProducts.mock.calls[0][0]).toBe("7");
+      expect(getProducts.mock.calls[0][1]).toBe("7");
+    });
+  });
+});
+
+describe("GET /products/:id", () => {
+  describe("request containing product id", () => {
+    test("should call getProduct with passed in argument of id", async () => {
+      const response = await request(app).get("/products/15000");
+      expect(getProduct.mock.calls.length).toBe(1);
+      expect(getProduct.mock.calls[0][0]).toBe("15000");
+    });
+  });
+});
+
+describe("GET /products/:id/styles", () => {
+  describe("request containing product id", () => {
+    test("should call getStyle with passed in argument of id", async () => {
+      const response = await request(app).get("/products/15000/styles");
+      expect(getStyle.mock.calls.length).toBe(1);
+      expect(getProduct.mock.calls[0][0]).toBe("15000");
+    });
+  });
 });
