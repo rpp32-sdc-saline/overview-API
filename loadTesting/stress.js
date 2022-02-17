e6126477742bda2081ce40bd6f90dea5da1414b3
Joseph Nahm <josephnahm1@gmail.com>
2022-02-16 16:30:42 -0800

    K6 test sends New Relic data with random ids in upper range

--- loadTesting/stress.js
+++ loadTesting/stress.js
@@ -1,22 +1,23 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
+const minId = 900000;
+const maxId = 1000012;
+
 export const options = {
   stages: [
-    { duration: "2m", target: 1 },
-    { duration: "2m", target: 10 },
-    { duration: "5m", target: 10 },
-    { duration: "2m", target: 100 },
-    { duration: "5m", target: 100 },
-    { duration: "2m", target: 1000 },
-    { duration: "5m", target: 1000 },
-    { duration: "10m", target: 0 },
+    { duration: "30s", target: 1 },
+    // { duration: "1m", target: 10 },
+    // { duration: "3m", target: 10 },
+    // { duration: "2m", target: 100 },
+    // { duration: "3m", target: 100 },
+    // { duration: "2m", target: 0 },
   ],
   insecureSkipTLSVerify: true,
   noConnectionReuse: false,
 };
 
 export default function () {
-  http.get("http://localhost:3000/overview/10000");
+  http.get(`http://localhost:3000/overview/${Math.floor(Math.random() * (maxId - minId) + minId)}`);
   sleep(1);
 }
