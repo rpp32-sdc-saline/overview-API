a1064d89a82c5620ccd5ef85df92fca59662ceb0
Joseph Nahm <josephnahm1@gmail.com>
2022-02-24 16:20:51 -0800

    increased range and performed both spike and stress test. Seems like Postgres has cached the entire database

--- loadTesting/stress.js
+++ loadTesting/stress.js
@@ -1,21 +1,23 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
-const minId = 900000;
+const minId = 0;
 const maxId = 1000012;
 
+//Stress Test
 export const options = {
   stages: [
-    { duration: "30s", target: 1 },
-    // { duration: "1m", target: 10 },
-    // { duration: "3m", target: 10 },
-    // { duration: "2m", target: 100 },
-    // { duration: "3m", target: 100 },
-    // { duration: "2m", target: 0 },
+    { duration: "1m", target: 1000 },
   ],
-  insecureSkipTLSVerify: true,
-  noConnectionReuse: false,
+  // insecureSkipTLSVerify: true,
+  // noConnectionReuse: false,
 };
+
+//Spike
+// export const options = {
+//   vus: 250,
+//   duration: '1m',
+// };
 
 export default function () {
   http.get(`http://localhost:3000/overview/${Math.floor(Math.random() * (maxId - minId) + minId)}`);
