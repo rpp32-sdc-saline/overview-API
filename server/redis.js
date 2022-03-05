d8ceebae0109f940ff2ff351ad93da63c46f3278
Joseph Nahm <josephnahm1@gmail.com>
2022-03-05 11:13:23 -0800

    deploy

--- server/redis.js
+++ server/redis.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
-// const redisHost = JSON.parse(process.env.IPS).redisIP;
-// const redisPwd = JSON.parse(process.env.IPS).redisPwd;
+const redisHost = JSON.parse(process.env.IPS).redisIP;
+const redisPwd = JSON.parse(process.env.IPS).redisPwd;
 
-// module.exports = redis.createClient({url: `redis://default:${redisPwd}@${redisHost}:6379`});
+module.exports = redis.createClient({url: `redis://default:${redisPwd}@${redisHost}:6379`});
 
-module.exports = redis.createClient(6379);
+// module.exports = redis.createClient(6379);
